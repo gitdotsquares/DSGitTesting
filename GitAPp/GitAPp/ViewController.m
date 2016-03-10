b98af181e1c3110711d945c9a44ea9eec9fbd009
sudanshu.pareek <sudhanshu@dotsquares.com>
2016-03-10 16:40:10 +0530

    Commit

--- GitAPp/GitAPp/ViewController.m
+++ GitAPp/GitAPp/ViewController.m
@@ -15,6 +15,7 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    // Hi My Name is Sumit
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
