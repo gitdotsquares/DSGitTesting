eb56dbff477431b3cceb13efce2da4cf7af22c7c
sudanshu.pareek <sudhanshu@dotsquares.com>
2016-03-10 16:34:49 +0530

    Commit 4.34
    Commit 4.34

---
+++ GitAPp/GitAPp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitAPp
+//
+//  Created by Sumit Shastri on 3/10/16.
+//  Copyright © 2016 ds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
